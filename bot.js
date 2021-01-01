5714abea74ae081a7f2069c02ddd47f9cb1db8c5
Easton Kang <euisungkang@gmail.com>
2021-01-01 17:59:32 -0600

    Second

--- bot.js
+++ bot.js
@@ -2,19 +2,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
 
 client.on('message', message => {
 
     if (message.content === 'ping') {
-
        message.reply('pong');
+    }
+    
+    if (message.content === 'pong') {
+        message.reply('ping');
+    }
 
-       }
+    if (message.content === '!introduce') {
+        message.reply('Wear a Mask, Stay Safe')
+    }
 
 });
 
